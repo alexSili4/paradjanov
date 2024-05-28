5f955a183e1a6f40afb8cbc36b4ed3097798ae49
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-05-28 14:06:53 +0300

    Update CarouselComponent.vue

--- src/components/CarouselComponent.vue
+++ src/components/CarouselComponent.vue
@@ -93,8 +93,87 @@ onUpdated(() => {
 }
 
 /*  */
+swiper-container {
+  width: 100%;
+  height: 100%;
+}
 
+swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+body {
+  background: #000;
+  color: #000;
+}
+
+swiper-container {
+  width: 100%;
+  height: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+swiper-slide {
+  background-size: cover;
+  background-position: center;
+}
+
+.mySwiper {
+  height: 80%;
+  width: 100%;
+}
+
+.mySwiper2 {
+  height: 20%;
+  box-sizing: border-box;
+  padding: 10px 0;
+}
+
+.mySwiper2 swiper-slide {
+  width: 25%;
+  height: 100%;
+  opacity: 0.4;
+}
+
+.mySwiper2 .swiper-slide-thumb-active {
+  opacity: 1;
+}
+
+swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
 /*  */
+.card-wrap {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.mySwiper img {
+  max-width: 100%;
+  max-height: 80%;
+  object-fit: contain;
+}
+
+.mySwiper swiper-slide {
+  background-color: transparent;
+}
 
 .text-wrap {
   display: flex;
